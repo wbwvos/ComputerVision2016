b78f5413b50c7d2649e7b20f602ede59fb345c0f
kb211 <kas_bouwens@hotmail.com>
2016-02-17 16:12:34 +0100

    plaatjes + color_spaces

--- Assignment1/color_spaces.m
+++ Assignment1/color_spaces.m
@@ -1,4 +1,4 @@
-function color_spaces(image)
+function color_spaces(image, color_space)
 
 img = imread(image); % Read image
 red = img(:,:,1); % Red channel
@@ -9,67 +9,89 @@ just_red = cat(3, red, a, a);
 just_green = cat(3, a, green, a);
 just_blue = cat(3, a, a, blue);
 
-figure
-subplot(2,2,1)
-subimage(img)
-title('Original image')
+switch color_space
+    case 'RGB'
+        figure
+        subplot(2,2,1)
+        subimage(img)
+        title('Original image')
 
-subplot(2,2,2)
-subimage(just_red)
-title('Red channel')
+        subplot(2,2,2)
+        subimage(just_red)
+        title('Red channel')
 
-subplot(2,2,3)
-subimage(just_green)
-title('Green channel')
+        subplot(2,2,3)
+        subimage(just_green)
+        title('Green channel')
 
-subplot(2,2,4)
-subimage(just_blue)
-title('Blue channel')
+        subplot(2,2,4)
+        subimage(just_blue)
+        title('Blue channel')
+    case 'opponent'
+        O1 = (red - green)./sqrt(2);
+        O2 = (red+green - 2.*blue)./sqrt(6);
+        O3 = (red+green+blue)./sqrt(3);
+        
+        figure
+        subplot(2,2,1)
+        subimage(O1)
+        title('O1')
 
-O1 = (red - green)./sqrt(2);
-O2 = (red+green - 2.*blue)./sqrt(6);
-O3 = (red+green+blue)./sqrt(3);
-opponent_img = cat(3, O1, O2, O3);
-figure
-subplot(2,2,1)
-subimage(O1)
-title('O1')
+        subplot(2,2,2)
+        subimage(O2)
+        title('O2')
 
-subplot(2,2,2)
-subimage(O2)
-title('O2')
+        subplot(2,2,3)
+        subimage(O3)
+        title('O3')
+        
+    case 'normRGB'
+        nomalized_red = red./(red+green+blue);
+        nomalized_green = green./(red+green+blue);
+        nomalized_blue = blue./(red+green+blue);
+        just_normalized_red = cat(3, nomalized_red, a, a);
+        just_nomalized_green = cat(3, a, nomalized_green, a);
+        just_nomalized_blue = cat(3, a, a, nomalized_blue);
+        normalized_img = cat(3, nomalized_red, nomalized_green, nomalized_blue);
 
-subplot(2,2,3)
-subimage(O3)
-title('O3')
+        figure
+        subplot(2,2,1)
+        subimage(just_normalized_red*255)
+        title('normalized red * 255')
 
-subplot(2,2,4)
-subimage(opponent_img)
-title('opponent colors')
+        subplot(2,2,2)
+        subimage(just_nomalized_green*255)
+        title('normalized green * 255')
 
-nomalized_red = red./(red+green+blue);
-nomalized_green = green./(red+green+blue);
-nomalized_blue = blue./(red+green+blue);
-just_normalized_red = cat(3, nomalized_red, a, a);
-just_nomalized_green = cat(3, a, nomalized_green, a);
-just_nomalized_blue = cat(3, a, a, nomalized_blue);
-normalized_img = cat(3, nomalized_red, nomalized_green, nomalized_blue);
+        subplot(2,2,3)
+        subimage(just_nomalized_blue*255)
+        title('normalized blue * 255')
 
-figure
-subplot(2,2,1)
-subimage(just_normalized_red*255)
-title('normalized_red')
+        subplot(2,2,4)
+        subimage(normalized_img*255)
+        title('normalized channels combined * 255')
+    case 'HSV'
+        hsv_image = rgb2hsv(img);
+       
+        figure
+        subplot(2,2,1)
+        subimage(hsv_image(:,:,1))
+        title('hue')
 
-subplot(2,2,2)
-subimage(just_nomalized_green*255)
-title('normalized_green')
+        subplot(2,2,2)
+        subimage(hsv_image(:,:,2))
+        title('saturation')
 
-subplot(2,2,3)
-subimage(just_nomalized_blue*255)
-title('normalized_blue')
-
-subplot(2,2,4)
-subimage(normalized_img*255)
-title('opponent colors')
+        subplot(2,2,3)
+        subimage(hsv_image(:,:,3))
+        title('value')
+        
+        subplot(2,2,4)
+        subimage(hsv_image)
+        title('image in HSV')
+        
+    otherwise 
+        disp('Type either: RGB, opponent, normRGB, HSV')
+end
 
 end
