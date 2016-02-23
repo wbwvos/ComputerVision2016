2668344ff8d7012298221b037b36a4e668210d92
kb211 <kas_bouwens@hotmail.com>
2016-02-23 18:04:39 +0100

    gaussianDer updated

--- assignment2/gaussianDer.m
+++ assignment2/gaussianDer.m
@@ -5,7 +5,15 @@ image = im2double(rgb2gray(imread(image_path)));
 x = linspace((-(kernelLength-1)/2),((kernelLength-1)/2), kernelLength);
 Gd = (-(x)./(sigma^2)).*G;
 %Gd = Gd./(sum(Gd));
-imOut = conv2(conv2(image, Gd, 'valid'), Gd', 'valid');
-%imOut = imOut/sum(sum(imOut));
+imOutX = conv2(image, Gd, 'valid');
+imOutY = conv2(image, Gd', 'valid');
+size(imOutX)
+size(imOutY)
+%imOut = sqrt(imOutX.^2 + imOutY.^2);
+
+imshow(im2double(imOutX), []);
+figure
+imshow(im2double(imOutY), []);
+figure
 imshow(im2double(imOut), []);
 end
