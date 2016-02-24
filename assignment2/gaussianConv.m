099eab62412ba3cf6fb733b1287feb6e2c458061
kb211 <kas_bouwens@hotmail.com>
2016-02-24 12:18:18 +0100

    plots 1.2 added

--- assignment2/gaussianConv.m
+++ assignment2/gaussianConv.m
@@ -6,8 +6,8 @@ image = im2double(image);
 kernel_x = gaussian(sigma_x, 11);
 kernel_y = gaussian(sigma_y, 11);
 
-image = conv2(image, kernel_x, 'valid');
-imOut = conv2(image, transpose(kernel_y), 'valid')
+image = conv2(image, kernel_x, 'same');
+imOut = conv2(image, transpose(kernel_y), 'same');
 
 
 end
