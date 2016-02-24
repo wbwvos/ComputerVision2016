aaccf8f328c4d5d72f5f504d012d66dfbe37da37
Wolf Vos <wolf@mijnlieff.nl>
2016-02-24 12:56:32 +0100

    2d kernel in commentaar toegevoegd

--- assignment2/gaussianConv.m
+++ assignment2/gaussianConv.m
@@ -6,6 +6,11 @@ image = im2double(image);
 kernel_x = gaussian(sigma_x, 11);
 kernel_y = gaussian(sigma_y, 11);
 
+%kernel_xy = conv2(kernel_x, kernel_y', 'full')
+%image2 = conv2(image, kernel_xy, 'same');
+%imshow(image2);
+%figure
+
 image = conv2(image, kernel_x, 'same');
 imOut = conv2(image, transpose(kernel_y), 'same');
 
