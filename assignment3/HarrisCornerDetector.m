e7354c9655264ee8cafa3ae9bcb230b63d7fe188
kb211 <kas_bouwens@hotmail.com>
2016-03-02 15:42:38 +0100

    HarrisCornerDetector

---
+++ assignment3/HarrisCornerDetector.m
@@ -0,0 +1,48 @@
+function HarrisCornerDetector()
+rawImage  = imread('person_toy/00000001.jpg');
+image = im2double(rgb2gray(rawImage));
+%image = im2double(rgb2gray(imread('pingpong/0000.jpg')));
+sigma = 2;
+G = fspecial('gauss',[1, 11], sigma);
+Gd = gradient(G);
+
+Ix = conv2(image, Gd, 'same');
+%A = conv2(Ix.^2, G, 'same');
+A = Ix.^2;
+
+Iy = conv2(image, Gd', 'same');
+%C = conv2(I.^2, G', 'same');
+C = Iy.^2;
+
+B = Ix.*Iy;
+H = (A.*C - B.^2) - 0.04*(A + C).^2;
+[imageHeight, imageWidth] = size(H);
+%imshow(H, []);
+
+windowWidth = 19;
+windowHeight = 19;
+
+coordinates = zeros(10, 3);
+counter =1;
+threshold = 1;
+for j = 1:imageHeight - windowHeight + 1
+    for i = 1:imageWidth - windowWidth + 1
+        window = image(j:j + windowHeight - 1, i:i + windowWidth - 1);
+        centrepoint = window(ceil(windowHeight/2), ceil(windowWidth/2));
+        if (centrepoint == max(max(window)) && centrepoint> 0.9)
+            y_max = j + ceil(windowHeight/2);
+            x_max = i + ceil(windowHeight/2);
+            coordinates(counter, 1) = y_max;
+            coordinates(counter, 2) = x_max;
+            coordinates(counter, 3) = centrepoint;
+            counter = counter+1;
+        end
+    end
+end
+[ncoordinates, values] = size(coordinates);
+for i = 1: ncoordinates
+    rawImage(coordinates(i, 1), coordinates(i, 2), 1) = 255;
+    rawImage(coordinates(i, 1), coordinates(i, 2), 2) = 0;
+    rawImage(coordinates(i, 1), coordinates(i, 2), 3) = 0;
+end
+imshow(rawImage)
