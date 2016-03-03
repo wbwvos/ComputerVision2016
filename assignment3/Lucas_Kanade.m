a95d6d7fd6862e6218044a037a18a4291cd6729b
Wolf Vos <wolf@mijnlieff.nl>
2016-03-03 17:50:21 +0100

    vector plots toegevoegd aan lucas kanade

--- assignment3/Lucas_Kanade.m
+++ assignment3/Lucas_Kanade.m
@@ -5,7 +5,7 @@ image1 = im2double(rgb2gray(rawImage1));
 rawImage2 = imread('sphere2.ppm');
 image2 = im2double(rgb2gray(rawImage2));
 
-[imageHeight, imageWidth] = size(image1);
+[imageHeight, imageWidth] = size(image2);
 windowHeight = 20;
 windowWidth = 20;
 
@@ -16,6 +16,9 @@ Ix = conv2(image2, Gd, 'same');
 Iy = conv2(image2, Gd', 'same');
 It = image2 - image1;
 
+noRegions = imageHeight/windowHeight;
+Vx = zeros([noRegions,noRegions]);
+Vy = zeros([noRegions,noRegions]);
 for j = 1:imageHeight/windowHeight
     for i = 1:imageWidth/windowWidth
         %Wimage1 = image1((j*windowHeight)-windowHeight +1:j*windowHeight -1, (i*windowWidth)-windowWidth+1:i*windowWidth -1);
@@ -25,8 +28,14 @@ for j = 1:imageHeight/windowHeight
         WIt = It((j*windowHeight)-windowHeight +1:j*windowHeight, (i*windowWidth)-windowWidth+1:i*windowWidth);
         A = [reshape(WIx2, [windowHeight*windowWidth, 1]), reshape(WIy2, [windowHeight*windowWidth, 1])];
         b = reshape(-WIt, [windowHeight*windowWidth, 1]);
-        v = pinv(A' * A) * A' * b
+        v = pinv(A' * A) * A' * b;
+        Vx(j,i) = v(1);
+        Vy(j,i) = v(2);
     end
 end
+[X, Y] = meshgrid([1:20:200],[1:20:200]);
+imshow(rawImage2);
+hold on
+quiver(X, Y, Vx, Vy)
 
 end
