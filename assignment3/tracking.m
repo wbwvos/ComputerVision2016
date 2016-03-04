189e678129bff6d47bb8007b2523cc8ee80a06d1
kb211 <kas_bouwens@hotmail.com>
2016-03-04 14:59:07 +0100

    tracking

--- assignment3/tracking.m
+++ assignment3/tracking.m
@@ -4,24 +4,36 @@ contents = dir('C:\Users\Kasper\Documents\GitHub\ComputerVision2016\assignment3\
 windowWidth  = 11;
 windowHeight = 11;
 windowBorder = floor(windowHeight/2);
-for i = 1:numel(contents)
+imcounter = 0;
+coordinates = 0;
+for i = 1:numel(contents)-1
   filename = contents(i).name;
-  [path, name, ext] = fileparts(filename)
+  [path, name, ext] = fileparts(filename);
+  
+  filename2 = contents(i+1).name;
+  [path2, name2, ext2] = fileparts(filename2);
   
   if ext == '.jpeg'
-  [H, coordinates] = HarrisCornerDetector(strcat('\pingpong\',name, ext));
+      imcounter = imcounter + 1;
+      if imcounter == 1
+        [H, coordinates] = HarrisCornerDetector(strcat('\pingpong\',name, ext));
+      end
+  image1 = strcat('\pingpong\',name, ext);
+  image2 = strcat('\pingpong\',name2, ext2);
+  newcoordinates = Lucas_Kanade2(image1, image2, coordinates);   
+  coordinates = newcoordinates;
   end
-  if i == 4
-      break
-  end
-  
+  %if i == 10
+  %    break
+  %end
 end
 
 
 end
 
 
-function Lucas_Kanade2(image1, image2, coordinates)
+function newcoordinates = Lucas_Kanade2(image1, image2, coordinates)
+
 rawImage1 = imread(image1);
 image1 = im2double(rgb2gray(rawImage1));
 
@@ -29,8 +41,9 @@ rawImage2 = imread(image2);
 image2 = im2double(rgb2gray(rawImage2));
 
 [imageHeight, imageWidth] = size(image2);
-windowHeight = 20;
-windowWidth = 20;
+windowHeight = 41;
+windowWidth = 41;
+windowBorder = floor(windowHeight/2);
 
 G = fspecial('gauss',[1, 7], 1);
 Gd = gradient(G);
@@ -54,9 +67,14 @@ for i = 1:m
   Vx(i,1) = v(1);
   Vy(i,1) = v(2);
 end
-
+figure
 imshow(rawImage2);
 hold on
-quiver(coordinates(:,1), coordinates(:,2), Vx, Vy)
+quiver(coordinates(:,2), coordinates(:,1), Vx, Vy)
 
+coordinates(:, 1) = coordinates(:,1)+ round(Vy);
+coordinates(:, 2) = coordinates(:,2)+ round(Vx);
+Vy
+Vx
+newcoordinates = coordinates
 end
