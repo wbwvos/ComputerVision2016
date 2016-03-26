55630bc02142b74765a193a56deddcc8d96b0ae4
kb211 <kas_bouwens@hotmail.com>
2016-03-26 18:39:45 +0100

    k-means

--- Final Project/FinalProject.m
+++ Final Project/FinalProject.m
@@ -1,24 +1,38 @@
 function BoW()
 run('vlfeat-0.9.20/toolbox/vl_setup')
+sigma = 1;
+k = 2;
 
-im = rgb2gray(imread('Caltech4/ImageData/motorbikes_train/img001.jpg'));
-im = single(im);
-binSize = 8 ;
-magnif = 3 ;
-Ismooth = vl_imsmooth(im, sqrt((binSize/magnif)^2 - .25)) ;
-[f, d] = vl_dsift(Ismooth, 'size', binSize) ;
-f(3,:) = binSize/magnif;
-f(4,:) = 0 ;
-[f_, d_] = vl_sift(im, 'frames', f);  
-[n,m] = size(d_);
+allDescriptors = []; 
 
-[idx, C] = kmeans(double(d), 4);
+folder = 'ImageData';
+directory = strcat(pwd, '\',folder,'\');
 
-%x1 = min(X(:,1)):0.01:max(X(:,1));
-%x2 = min(X(:,2)):0.01:max(X(:,2));
-%[x1G,x2G] = meshgrid(x1,x2);
-%XGrid = [x1G(:),x2G(:)]; % Defines a fine grid on the plot
+subdirs = dir(directory);
+for i=1:length(subdirs)
+    path = strcat(directory ,subdirs(i).name);
+    fileCheck = strcat(path, '\img400.jpg');
+    if exist(fileCheck) == 2
+        files = dir(path);
+        for j=1:length(files) 
+            filename = strcat(path, '\', files(j+3).name)
+            im = rgb2gray(imread(filename));
+            im = single(im);
+            
+            Is = vl_imsmooth(im, sigma) ;
+            [frames, descriptors] = vl_dsift(Is) ;
 
-%idx2Region = kmeans(XGrid,3,'MaxIter',1,'Start',C);
-    % Assigns each node in the grid to the closest centroid
+            %[frames, descriptors] = vl_sift(im);  
+            if i == 4 && j == 1
+                allDescriptors = descriptors;
+            else
+            allDescriptors = [allDescriptors descriptors];
+            end
+            break
+            
+        end
+    end
+end
+descriptors = double(allDescriptors);
+[idx, C] = kmeans(allDescriptors, k);
 end
