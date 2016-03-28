90c8a5fe5c2f89d0b7df14fe8b781e770d0d0257
kb211 <kas_bouwens@hotmail.com>
2016-03-28 21:16:12 +0200

    compute histogram

--- Final Project/computeHistogram.m
+++ Final Project/computeHistogram.m
@@ -5,23 +5,18 @@ if size(im, 3) > 1
 end
 im = single(im);
 histogram = zeros(1, 400);
+differences = zeros(size(C));
 [frames, descriptors] = vl_sift(im);
 descriptors = double(descriptors);
 
-smallest = 10000000;
-cluster = 0;
 for i = 1:size(descriptors,2)
     for j = 1:size(C,2)
-        difference = norm(descriptors(:,i) - C(:,j));
-        if difference < smallest
-            smallest = difference;
-            cluster = j;
-        end
-        %break
+        differences(1,j) = norm(descriptors(:,i) - C(:,j));
     end
-    
-    histogram(cluster) = histogram(cluster) + 1;
+    smallest = min(differences);
+    normpdf(differences, 0, smallest);
+    histogram = histogram+differences;
 end
 
-
+histogram = histogram/sum(histogram);
 end
